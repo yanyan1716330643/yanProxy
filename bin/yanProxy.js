aac1effdb00cdaa2cf605b15186426e4d709e92f
yanyan <1716330643@qq.com>
2017-12-07 15:30:18 +0800

    first

--- bin/yanProxy.js
+++ bin/yanProxy.js
@@ -4,9 +4,11 @@ let httpProxy = require("http-proxy");
 let proxy = httpProxy.createProxy({});
 let filePath = (process.argv && process.argv[2]) || "../proxyConfig.js";
 let path = require("path");
+console.log("config path:");
 console.log(path.resolve(filePath));
 let proxyConfig = require(path.resolve(filePath));
 // console.log(process.env)
+console.log("config content:")
 console.log(proxyConfig);
 for(let obj of proxyConfig){
     let server = http.createServer(function (req, res) {
