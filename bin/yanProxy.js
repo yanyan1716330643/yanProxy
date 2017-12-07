024b7653435646c95e359c2f5fd6c3b624bde0f1
yanyan <1716330643@qq.com>
2017-12-07 14:49:13 +0800

    first

---
+++ bin/yanProxy.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+let http = require("http");
+let httpProxy = require("http-proxy");
+let proxy = httpProxy.createProxy({});
+let filePath = (process.argv && process.argv[2]) || "../proxyConfig.js";
+let path = require("path");
+console.log(path.resolve(filePath));
+let proxyConfig = require(path.resolve(filePath));
+// console.log(process.env)
+console.log(proxyConfig);
+for(let obj of proxyConfig){
+    let server = http.createServer(function (req, res) {
+        proxy.web(req,res,{target:obj.targetUrl});
+    });
+    console.log(`port:${obj.port} targetUrl:${obj.targetUrl}`);
+    server.listen(obj.port);
+}
+console.log("yanProxy is running...");
---
+++ proxyConfig.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+        port:6000,
+        targetUrl:'http://127.0.0.1:8080'
+    },
+    {
+        port:6001,
+        targetUrl:'http://192.168.1.2:8088'
+    },
+    {
+        port:6003,
+        targetUrl:'http://lonelyshadow.cn/'
+    },
+
+];
